8aa790b68ba5aa4a88c9b47effd66c671b497f6e
zebus3d <zebus3dream@gmail.com>
2019-05-12 22:26:12 +0200

    poca cosa

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from rws import ReaderWritterSettigns
 rw = ReaderWritterSettigns()
 # voy a leer en la seccion defaults la opcion BuilderServer:
 rw.reader('defaults', 'BuilderServer')
+
+rw.add_section('test') 
+rw.add_option('test', 'pepe', 'volador')
+rw.write_options()
--- rws.py
+++ rws.py
@@ -6,11 +6,20 @@ sf = "settings.ini"
 class ReaderWritterSettigns():
     # constructor:
     def __init__(self):
-        self._Config = configparser.ConfigParser()
-        self._sections = self._Config.sections()
-        self._Config.read(sf)
-
+        self._config = configparser.ConfigParser()
+        self._sections = self._config.sections()
+        self._config.read(sf)
 
     # metodos:
     def reader(self, section, option):
-        print(self._Config.get(section, option))
+        print(self._config.get(section, option))
+
+    def add_section(self, new_section):
+        self._config.add_section(new_section)
+
+    def add_option(self, section, option, value):
+        self._config.set(section, option, value)
+    
+    def write_options(self):
+        with open(sf, 'w') as configfile:
+            self._config.write(configfile)
