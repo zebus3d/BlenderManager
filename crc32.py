489e0f5bc8de269ef329263970d1c079ab68d1e4
zebus3d <zebus3dream@gmail.com>
2019-05-15 17:08:39 +0200

    verificar con crc32, parecido a md5sum pero rápido

---
+++ crc32.py
@@ -0,0 +1,12 @@
+import zlib
+
+
+def crc32_checker(fileName):
+    prev = 0
+    for eachLine in open(fileName,"rb"):
+        prev = zlib.crc32(eachLine, prev)
+    return "%X"%(prev & 0xFFFFFFFF)
+
+
+crc = crc32_checker('settings.cfg')
+print(crc)
