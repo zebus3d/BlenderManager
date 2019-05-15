5d44fbcab75fe4a11a6bfbffffb9787f14a28014
zebus3d <zebus3dream@gmail.com>
2019-05-15 17:51:47 +0200

    nada grave

--- src/services/crc32.py
+++ src/services/crc32.py
@@ -1,12 +1,16 @@
 import zlib
-
+import os
 
 def crc32_checker(fileName):
-    prev = 0
-    for eachLine in open(fileName,"rb"):
-        prev = zlib.crc32(eachLine, prev)
-    return "%X"%(prev & 0xFFFFFFFF)
+    if os.path.isfile(fileName):
+        prev = 0
+        for eachLine in open(fileName,"rb"):
+            prev = zlib.crc32(eachLine, prev)
+        return "%X"%(prev & 0xFFFFFFFF)
+    else:
+        print("No se encontro el archivo: " + str(fileName))
 
 
-crc = crc32_checker('../data/settings.cfg')
-print(crc)
+crc = crc32_checker('../../data/settings.cfg')
+if crc:
+    print(crc)
