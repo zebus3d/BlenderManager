102bb2c5fcd1a5fb7e2eee73c96ab02499c21b3a
Damasko <damasko015@yahoo.es>
2019-05-15 21:55:43 +0200

    Añadido servicio DownloaderService v1

---
+++ src/model/BlenderFile.py
@@ -0,0 +1,12 @@
+
+class BlenderFile:
+
+    def __init__(self, name):
+
+        self.name = name
+        self.version = ""
+        self.builds = ["stable"]
+
+    def addBuild(self, build):
+
+        self.builds.append(build)
