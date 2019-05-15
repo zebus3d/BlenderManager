a2171734a02498be07ee75cb1144116e266720ae
zebus3d <zebus3dream@gmail.com>
2019-05-15 16:17:33 +0200

    poca cosa

--- main.py
+++ main.py
@@ -18,9 +18,17 @@ class Screen02(Screen):
     pass
 
 # Create the screen manager
-# sm = ScreenManager()
+# NoTransition - switches screens instantly with no animation
+# SlideTransition - slide the screen in/out, from any direction
+# CardTransition - new screen slides on the previous or the old one slides off the new one depending on the mode
+# SwapTransition - implementation of the iOS swap transition
+# FadeTransition - shader to fade the screen in/out
+# WipeTransition - shader to wipe the screens from right to left
+# FallOutTransition - shader where the old screen ‘falls’ and becomes transparent, revealing the new one behind it.
+# RiseInTransition - shader where the new screen rises from the screen centre while fading from transparent to opaque.
+sm = ScreenManager()
 # sm = ScreenManager(transition=NoTransition())
-sm = ScreenManager(transition=FadeTransition())
+# sm = ScreenManager(transition=FadeTransition())
 sm.add_widget(Screen01(name='home'))
 sm.add_widget(Screen02(name='screen2'))
 
