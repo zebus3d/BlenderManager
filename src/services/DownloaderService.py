6d9c3a569a65da260fbea01d4bb4c357bd13c08c
zebus3d <zebus3dream@gmail.com>
2019-05-16 00:26:03 +0200

    ya tengo el listado de nombre de version y fechas

--- src/services/DownloaderService.py
+++ src/services/DownloaderService.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from src.model.BlenderFile import BlenderFile
 
+import re
+
+import os
+print(os.getcwd())
+
+# from src.model.BlenderFile import BlenderFile
+
+import sys
+sys.path.insert(0, 'src/model/')
+from BlenderFile import BlenderFile
 
 class DownloaderService:
 
@@ -11,20 +20,33 @@ class DownloaderService:
     def getAvaibleVersions(self):
 
         url = "https://download.blender.org/release/"
-        tmpResult = []
+        blendObj = []
 
         html_page = urlopen(url)
         soup = BeautifulSoup(html_page)
-        for link in soup.findAll('a'):
-            
-            print(link)
-            # blendName = link.get('href')[:-1]
 
-        #     if str.startswith(blendName, 'Blender'):
-        #         bf = BlenderFile(blendName)
-        #         bf.version = blendName[7:11]
-        #         tmpResult.append(bf)
+        regexRule = '(Blender[0-9].[0-9]*).*( [0-9]*-[a-zA-Z]*-[0-9]* [0-9]*:[0-9]*)'
 
-        # for item in tmpResult:
+        data = re.findall(regexRule, str(soup))
+        print(data)
 
+
+        # for link in soup.findAll('a'):
+
+        #     blendName = link.get('href')[:-1]
+
+        #     print(blendName)
+
+        #     if blendName.startswith('Blender'):
+        #         if not "Benchmark" in blendName:
+        #             bf = BlenderFile(blendName)
+        #             bf.version = blendName[7:11]
+        #             blendObj.append(bf)
+
+        # for item in blendObj:
         #     print(str(item.name) + "  " + str(item.version) + " " + str(item.builds))
+
+
+if __name__ == "__main__":    
+    test = DownloaderService()
+    test.getAvaibleVersions()
