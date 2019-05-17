297c8c6699cf286fce8923f9e59900076de8ac66
zebus3d <zebus3dream@gmail.com>
2019-05-17 21:35:52 +0200

    intentando hacer dropdown...

--- Tests/main.py
+++ Tests/main.py
@@ -1,16 +1,36 @@
 from kivy.app import App
 from kivy.uix.dropdown import DropDown
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
 from kivy.lang import Builder
 
 Builder.load_file('test_ui.kv')
 
-class CustomDropDown(BoxLayout):
+
+class CustomDropDown(DropDown):
     pass
 
 class MainApp(App):
     def build(self):
-        return CustomDropDown()
+        
+        # dropdown = CustomDropDown()
+        # mainbutton = Button(text='Hello', size_hint=(None, None))
+        # mainbutton.bind(on_release=dropdown.open)
+        # dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+        
+        dropdown = DropDown()
+
+        for index in range(10):
+            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+
+        # create a big main button
+        mainbutton = Button(text='Hello', size_hint=('1dp', None))
+        mainbutton.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+        return mainbutton
 
 if __name__=='__main__':
     MainApp().run()
