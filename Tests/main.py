158e7de913dc2ac807717f708905cb80cc2bba25
zebus3d <zebus3dream@gmail.com>
2019-05-17 21:59:19 +0200

    porfin funciona un combobox simple

--- Tests/main.py
+++ Tests/main.py
@@ -1,4 +1,6 @@
 from kivy.app import App
+from kivy.core.window import Window
+
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 
@@ -18,19 +20,21 @@ class MainApp(App):
         # mainbutton.bind(on_release=dropdown.open)
         # dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
         
-        dropdown = DropDown()
+        self.dropdown = DropDown()
 
         for index in range(10):
-            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-            dropdown.add_widget(btn)
+            btn = Button(text='Value %d' % index, size_hint_y=None, height=30)
+            btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
+            self.dropdown.add_widget(btn)
 
         # create a big main button
-        mainbutton = Button(text='Hello', size_hint=('1dp', None))
-        mainbutton.bind(on_release=dropdown.open)
-        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+        mainbutton = Button(text='Hello', size_hint=('1dp', None), pos=(0, 470), height=30)
+        mainbutton.bind(on_release=self.dropdown.open)
+        self.dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
 
         return mainbutton
 
 if __name__=='__main__':
-    MainApp().run()
+    app = MainApp()
+    Window.size = (200, 500)
+    app.run()
