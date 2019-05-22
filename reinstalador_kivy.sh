adf0a5b5b548ce03792df0b27bfe2971b6fdbfa0
zebus3d <zebus3dream@gmail.com>
2019-05-22 20:49:53 +0200

    poca cosa

--- reinstalador_kivy.sh
+++ reinstalador_kivy.sh
@@ -4,14 +4,14 @@
 # Estoy usando kyvy 1.10.1
 # Estoy usando Cython 0.28.6
 
-sudo aptitude purge python3-kivy python3-sdl2 python-pip python3-pip
+sudo apt purge python3-kivy python3-sdl2 python-pip python3-pip
 #pip install --upgrade pip --user
 # sudo pip3 uninstall cython kivy --user
-sudo aptitude reinstall python python3
+sudo apt reinstall python python3
 sudo apt autoremove
 
-sudo aptitude install python3-pip
+sudo apt install python3-pip
 
 sudo add-apt-repository ppa:kivy-team/kivy
-sudo apt-get update
-sudo apt-get install python3-kivy
+sudo apt update
+sudo apt install python3-kivy
