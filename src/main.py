c3dffd45588fe12158f9b38ea6be08c473b77ae1
zebus3d <48990185+zebus3d@users.noreply.github.com>
2023-05-13 00:44:49 +0200

    avanzando y refactor

--- src/main.py
+++ src/main.py
@@ -1,39 +1,21 @@
+import os
+import sys
+import configparser
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.core.window import Window
-from services.detector import OSDetector
-import os
-import sys
 from kivy.modules import inspector
+from services.osdetector import detect
+from mainscreen import MainScreen
+
 
 # Para que las builds de pyinstaller onefile encuentren los paths dentro del bundle:
 base_path = getattr(sys, "_MEIPASS", None)
 if base_path is not None:
     os.chdir(base_path)
 
-Window.size = (600, 400)
-Window.top = Window.top+50
-Window.left = Window.left-600/3.6
-Window.clearcolor = (66/255.0, 66/255.0, 66/255.0, 1)
-
-Builder.load_file('views/gui.kv')
-
-detector = OSDetector()
-sysi = detector.detect()
-
-class MainScreen(Screen):
-    def set_platform_name(self) -> str:
-        print("auto detect os: ", sysi.os)
-        return sysi.os or "current system not supported"
-
-    def set_architecture(self) -> str:
-        print("auto detect architecture: ", sysi.arch)
-        return sysi.arch or "err"
-
-    def print_current(self, current_value):
-        print(current_value)
 
 class PreferencesScreen(Screen):
     pass
@@ -41,15 +23,43 @@ class PreferencesScreen(Screen):
 class MyTabbedPanel(TabbedPanel):
     pass
 
+# Carga el archivo KV
+Builder.load_file('views/gui.kv')
+
+# Detecta el sistema operativo y la arquitectura
+sysi = detect()
+
+# Define la pantalla principal
+mainscreen = MainScreen(name='main_screen')
+
+# Agrega las pantallas al manejador de pantallas (ScreenManager)
 sm = ScreenManager(transition=NoTransition())
-sm.add_widget(MainScreen(name='main_screen'))
+sm.add_widget(mainscreen)
 sm.add_widget(PreferencesScreen(name='settings'))
 
+# Crea un inspector para la ventana principal
 inspector.create_inspector(Window, sm)
 
+# Define la aplicación principal
 class MainApp(App):
     def build(self):
-        self.title = 'Blender Downloader'
+        # Lee las configuraciones desde el archivo ini
+        config = configparser.ConfigParser()
+        config.read('src/config.ini')
+        self.title = config['app']['title']
+        Window.size = (int(config['app']['width']), int(config['app']['height']))
+
+        r, g, b, a= config['app']['background_color'].split(',')
+        red = float(r) / 255.0
+        green = float(g)
+        blue = float(b)
+        alpha = float(a)
+        Window.clearcolor = (red, green, blue, alpha)
+        
+        # Centra la ventana
+        if self.root_window:
+            Window.center = self.root_window.get_rect().center
+        
         return sm
 
 if __name__ == '__main__':
