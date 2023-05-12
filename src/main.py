11d1e67972dbcdf2645cd4d562da51d6d16c8d58
zebus3d <48990185+zebus3d@users.noreply.github.com>
2023-05-12 23:54:16 +0200

    mejorando un poco el código

--- src/main.py
+++ src/main.py
@@ -1,88 +1,57 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition # , SlideTransition , FadeTransition, 
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.core.window import Window
-import os, sys
 from services.detector import OSDetector
-
-# Para el inspector:
+import os
+import sys
 from kivy.modules import inspector
 
-
-# para que las builds de pyinstaller onefile encuentren los paths dentro del bundle:
-if hasattr(sys, "_MEIPASS"):
-    base_path = sys._MEIPASS
+# Para que las builds de pyinstaller onefile encuentren los paths dentro del bundle:
+base_path = getattr(sys, "_MEIPASS", None)
+if base_path is not None:
     os.chdir(base_path)
+
+Window.size = (600, 400)
+Window.top = Window.top+50
+Window.left = Window.left-600/3.6
+Window.clearcolor = (66/255.0, 66/255.0, 66/255.0, 1)
 
 Builder.load_file('views/gui.kv')
 
 detector = OSDetector()
 sysi = detector.detect()
 
-
 class MainScreen(Screen):
     def set_platform_name(self) -> str:
         print("auto detect os: ", sysi.os)
-        if sysi.os:
-            return sysi.os
-        else:
-            return "current system not supported"
+        return sysi.os or "current system not supported"
 
     def set_architecture(self) -> str:
         print("auto detect architecture: ", sysi.arch)
-        if sysi.arch:
-            return sysi.arch
-        else:
-            return "err"
+        return sysi.arch or "err"
 
     def print_current(self, current_value):
         print(current_value)
 
-
 class PreferencesScreen(Screen):
     pass
 
-
-class TabbedPanel(TabbedPanel):
+class MyTabbedPanel(TabbedPanel):
     pass
 
-
-# Screen manager
-sm = ScreenManager()
-# Effectos de transicion:
 sm = ScreenManager(transition=NoTransition())
-# sm = ScreenManager(transition=FadeTransition())
-# sm = ScreenManager(transition=SlideTransition())
+sm.add_widget(MainScreen(name='main_screen'))
+sm.add_widget(PreferencesScreen(name='settings'))
 
-# Inspector:
-# Para mostrar el inspector hay que pulsar ctrl + e
-# Si seleccionas un item dale al boton grande (a la izquierda del boton x ) para expandir sus propiedades:
 inspector.create_inspector(Window, sm)
-
-
-s01 = MainScreen(name='main_screen')
-s02 = PreferencesScreen(name='settings')
-
-
-sm.add_widget(s01)
-sm.add_widget(s02)
-
-
 
 class MainApp(App):
     def build(self):
         self.title = 'Blender Downloader'
         return sm
 
-
 if __name__ == '__main__':
     app = MainApp()
-    Window.size = (600, 400)
-    # centrando ventana:
-    Window.top = Window.top+50
-    Window.left = Window.left-600/3.6
-    # pongo este color por si usas transiciones que queden bien:
-    # el color de background donde no ocupa por completo un screen:
-    Window.clearcolor = (66/255.0, 66/255.0, 66/255.0, 1)
     app.run()
